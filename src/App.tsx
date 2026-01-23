2a1a6c8ed2e9aca2858713a9aaab77c392310f02
abdullahelfatnassi-coder <abdullahelfatnassi@gmail.com>
2026-01-23 18:29:52 +0000

    Coming soon page-app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export default function App() {
     setLoading(true);
 
     try {
-      const res = await fetch("https://formspree.io/f/YOUR_FORM_ID", {
+      const res = await fetch("https://formspree.io/f/mvzkqdna", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
