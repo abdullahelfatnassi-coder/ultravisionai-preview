00abb69b75dea953c0d43d97bc4ea7c1861b7ee7
abdullahelfatnassi-coder <abdullahelfatnassi@gmail.com>
2026-02-05 21:52:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const lock = document.querySelector(".lock");
+const app = document.querySelector(".app");
+
+lock.addEventListener("click", () => {
+  lock.style.opacity = "0";
+  setTimeout(() => {
+    lock.style.display = "none";
+    app.classList.add("unlocked");
+  }, 600);
+});
 
